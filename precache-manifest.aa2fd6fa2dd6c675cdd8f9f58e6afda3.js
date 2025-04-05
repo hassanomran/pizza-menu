99aa873be84eae1d540579b646f6eccbc597aa09
Hassan Omran <Hassan Omran@DESKTOP-A2A618B>
2025-04-05 15:53:10 +0200

    Updates

---
+++ precache-manifest.aa2fd6fa2dd6c675cdd8f9f58e6afda3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2bba83ed7fcf6582385de872f5643ea",
+    "url": "/pizza-menu/index.html"
+  },
+  {
+    "revision": "012660a66d140e4b316f",
+    "url": "/pizza-menu/static/css/main.e1c39e69.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/pizza-menu/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/pizza-menu/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "012660a66d140e4b316f",
+    "url": "/pizza-menu/static/js/main.8f30b282.chunk.js"
+  },
+  {
+    "revision": "52952a1ac9293af7c473",
+    "url": "/pizza-menu/static/js/runtime~main.7cb45084.js"
+  }
+]);
